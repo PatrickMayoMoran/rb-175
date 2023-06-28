1f08aaf121e2e818e3f227af952e47f52447f830
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-06-28 17:26:01 +0000

    Add desired outputs for parsing sample http request

--- server.rb
+++ server.rb
@@ -6,7 +6,19 @@ loop do
 
   request_line = client.gets
   puts request_line
+  "GET /_staticlocalhost:8080/?rolls=2&sides=6& HTTP/1.1"
+  request_components = parse(request_line)
+  http_method = request_components[:method]
+  path = request_components[:path]
+  query_parameters = request_components[:query_parameters]
+
+  http_method == "GET"
+  path == "/"
+  query_parameters == { "rolls"=>"2", "sides"=>"6"}
 
   client.puts request_line
   client.close
+end
+
+def parse(request_line)
 end
