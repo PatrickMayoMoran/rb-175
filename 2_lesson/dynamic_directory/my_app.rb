ec7f33d29f8efa153cad433e590c7dd78b91377f
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-07-07 14:10:01 +0000

    Add reversing of file list based on sort parameter

--- 2_lesson/dynamic_directory/my_app.rb
+++ 2_lesson/dynamic_directory/my_app.rb
@@ -1,9 +1,9 @@
 require 'sinatra'
 require 'sinatra/reloader'
-require 'erubis'
+require 'tilt/erubis'
 
 get '/' do
   @files = Dir.glob('./*').map {|file| File.basename(file) }.sort
-
+  @files.reverse! if params[:sort] == "desc"
   erb :home
 end
