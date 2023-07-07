4db32dbfcdab5c9de4e7c24ac55e6f3cda22cd68
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-07-07 15:16:39 +0000

    Update chapters route to use numbers parameter

--- 2_lesson/book_viewer/book_viewer.rb
+++ 2_lesson/book_viewer/book_viewer.rb
@@ -8,10 +8,10 @@ get "/" do
   erb :home
 end
 
-get "/chapters/1" do
-  @title = "Chapter 1"
+get "/chapters/:number" do
+  @title = "Chapter #{params[:number]}"
   @table_of_contents = File.readlines('data/toc.txt')
-  @chapter_1 = File.read('data/chp1.txt')
+  @chapter = File.read("data/chp#{params[:number]}.txt")
 
   erb :chapter
 end
