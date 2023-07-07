70c086a3f8677f69f56cbd4ace9ce29742192c60
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-07-07 14:25:36 +0000

    Change application to show list of public directory contents

--- 2_lesson/dynamic_directory/my_app.rb
+++ 2_lesson/dynamic_directory/my_app.rb
@@ -3,7 +3,7 @@ require 'sinatra/reloader'
 require 'tilt/erubis'
 
 get '/' do
-  @files = Dir.glob('./*').map {|file| File.basename(file) }.sort
+  @files = Dir.glob('public/*').map {|file| File.basename(file) }.sort
   @files.reverse! if params[:sort] == "descending"
   erb :home
 end
