31715e15bd51567740f397fb68c7584250315522
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-07-07 14:11:14 +0000

    Correct descending typo

--- 2_lesson/dynamic_directory/my_app.rb
+++ 2_lesson/dynamic_directory/my_app.rb
@@ -4,6 +4,6 @@ require 'tilt/erubis'
 
 get '/' do
   @files = Dir.glob('./*').map {|file| File.basename(file) }.sort
-  @files.reverse! if params[:sort] == "desc"
+  @files.reverse! if params[:sort] == "descending"
   erb :home
 end
