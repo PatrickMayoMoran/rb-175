e55e7bb0ac641daae82462c7793487a83396774a
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-07-08 17:33:19 +0000

    Change chapter view to display chapter number and chapter title

--- 2_lesson/book_viewer/book_viewer.rb
+++ 2_lesson/book_viewer/book_viewer.rb
@@ -9,8 +9,12 @@ get "/" do
 end
 
 get "/chapters/:number" do
-  @title = "Chapter #{params[:number]}"
   @table_of_contents = File.readlines('data/toc.txt')
+
+  chapter_number = params[:number].to_i
+  chapter_name = @table_of_contents[chapter_number - 1]
+  @title = "Chapter #{chapter_number}: #{chapter_name}"
+
   @chapter = File.read("data/chp#{params[:number]}.txt")
 
   erb :chapter
