600f1409eb0e1c9d8785a9a6d733754c612f364e
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-07-11 23:39:28 +0000

    Refactor loading of YAML file into a before block

--- 2_lesson/users_and_interests/my_app.rb
+++ 2_lesson/users_and_interests/my_app.rb
@@ -1,7 +1,15 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
+require 'yaml'
+
+before do
+  @users = YAML.load_file("users.yml")
+end
 
 get '/' do
-  "Users!"
+end
+
+helpers do
+  
 end
