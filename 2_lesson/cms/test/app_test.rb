5f3bf14d5d3fa7747f2a39267b13589999b4661b
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-08-01 21:12:34 +0000

    Add test file for CMS app

---
+++ 2_lesson/cms/test/app_test.rb
@@ -0,0 +1,14 @@
+ENV["RACK_ENV"] = "test"
+
+require 'minitest/autorun'
+require 'rack/test'
+
+require_relative "../cms.rb"
+
+class AppTest < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+end
